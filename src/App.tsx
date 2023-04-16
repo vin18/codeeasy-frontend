76e68fadeff84c9e5d05b3a24aa0747b4a6ea166
Vinit Raut <vinit.coder@gmail.com>
2023-04-16 17:49:29 +0530

    Add: Product listing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query';
 
 import Home from './pages/Home';
 import Products from './pages/Products';
+import { Container } from '@chakra-ui/react';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="products" element={<Products />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Container maxW="1200px">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="products" element={<Products />} />
+          </Routes>
+        </Container>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,69 @@
-import React from 'react';
+import {
+  Box,
+  Card,
+  CardBody,
+  Flex,
+  Heading,
+  Image,
+  SimpleGrid,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
+
+interface IProduct {
+  _id: string;
+  name: string;
+  description: string;
+  category: string;
+  image: {
+    public_id: string;
+    url: string;
+  };
+  price: number;
+  brand: string;
+  averageRating: number;
+  countInStock: number;
+}
 
 const Home = () => {
-  return <div>Home</div>;
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      fetch('http://localhost:5000/api/v1/products').then((res) => res.json()),
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <SimpleGrid margin="50px 0" minChildWidth="300px" spacing="40px">
+        {data?.map((p: IProduct) => (
+          <Card
+            _hover={{ backgroundColor: 'gray.50', transition: 'all' }}
+            cursor="pointer"
+            maxW="md"
+            key={p._id}
+          >
+            <CardBody>
+              <Image src={p.image.url} alt={p.name} borderRadius="lg" />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">{p.name}</Heading>
+                <Flex justifyContent="space-between">
+                  <Text color="blue.600" fontSize="xl">
+                    ₹{p.price}
+                  </Text>
+                  <Text color="blue.600" fontSize="xl">
+                    {p.brand}
+                  </Text>
+                </Flex>
+              </Stack>
+            </CardBody>
+          </Card>
+        ))}
+      </SimpleGrid>
+    </div>
+  );
 };
 
 export default Home;
